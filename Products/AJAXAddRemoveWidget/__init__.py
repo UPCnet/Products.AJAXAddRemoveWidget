9cecdd764b37fc9a6cf0e0cbb1818ecbe3c80e0e
carles.bruguera <carles.bruguera@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-07-16 12:28:06 +0000

    Primera versio AJAXAddRemoveWidget
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/Products.AJAXAddRemoveWidget@135 6deaf63b-a510-49cf-aad4-8c651776ecce

---
+++ Products/AJAXAddRemoveWidget/__init__.py
@@ -0,0 +1,10 @@
+from Products.CMFCore import utils
+from Products.CMFCore.DirectoryView import registerDirectory
+from config import *
+
+registerDirectory(SKINS_DIR, GLOBALS)
+
+from AJAXAddRemoveWidget import AJAXAddRemoveWidget
+
+def initialize(context):
+    pass
---
+++ Products/AJAXAddRemoveWidget/config.py
@@ -0,0 +1,6 @@
+# just to be compatible with the general AT approach
+
+PROJECTNAME = "AJAXAddRemoveWidget"
+SKINS_DIR = 'skins'
+GLOBALS = globals()
+
