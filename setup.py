e5623d1f2905c9a76c2039fce06608e9f9a1c20c
carles.bruguera <carles.bruguera@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-07-21 10:08:06 +0000

    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/Products.AJAXAddRemoveWidget@142 6deaf63b-a510-49cf-aad4-8c651776ecce

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='Products.AJAXAddRemoveWidget',
+      version=version,
+      description="AddRemoveWidget with autocompletion for Plone 3.x",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='addremovewidget ajax plone3',
+      author='Carles Bruguera',
+      author_email='carles.bruguera_at_upcnet.es',
+      url='http://svn.plone.org/svn/plone/plone.example',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
