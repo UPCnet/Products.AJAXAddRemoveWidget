d3678f003122d1eb827f191ae8e9bf39df7b30a0
Ariel <ariel.von.barnekow@upcnet.es>
2011-09-27 11:37:22 +0200

    Traduccions a locales

--- Products/AJAXAddRemoveWidget/__init__.py
+++ Products/AJAXAddRemoveWidget/__init__.py
@@ -1,8 +1,8 @@
-from Products.CMFCore import utils
-from Products.CMFCore.DirectoryView import registerDirectory
-from config import *
+#from Products.CMFCore import utils
+#from Products.CMFCore.DirectoryView import registerDirectory
+#from config import *
 
-registerDirectory(SKINS_DIR, GLOBALS)
+#registerDirectory(SKINS_DIR, GLOBALS)
 
 from AJAXAddRemoveWidget import AJAXAddRemoveWidget
 
